bc295c7d0e248957deeaec315e0c128143d1e4be
jose16201 <jose_16201@hotmail.com>
2015-11-05 02:33:49 +0000

    clase 3

---
+++ app/models/user.rb
@@ -0,0 +1,59 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+         
+  has_one :profile
+
+
+def default_profile
+
+  if self.profile.nil?
+
+    profile = Profile.new
+
+    profile.user = self
+
+    profile.save
+
+    self.save      
+
+  end
+
+  self.profile
+
+end
+
+enum role: [:user, :operator, :chef, :admin]
+
+
+after_initialize :set_default_role
+
+
+def set_default_role
+
+  self.role ||= :user
+
+end
+         
+         
+  def friendly_name
+
+    if self.default_profile.first_name.nil?
+
+      self.email
+
+    else
+
+      self.profile.full_name
+
+    end
+
+ end  
+         
+         
+end
+
+
